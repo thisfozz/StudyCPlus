b60a68c5e804aa708d9befc17bab9870ce21e6b0
noanonofi@yandex.ru <noanonofi@yandex.ru>
2023-01-21 15:16:22 +0500

    Задание про мороженное

--- StudyCPlus/StudyCPlus.cpp
+++ StudyCPlus/StudyCPlus.cpp
@@ -2,5 +2,18 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int minK{ 3 };
+    const int maxK{ 5 };
+    int value{ 0 };
+
+    std::cout << "Enter value: ";
+    std::cin >> value;
+
+    system("cls");
+
+    //******************************************************************
+    bool isBool;
+    isBool = (value == 1) || (value == 2) || (value == 4) || (value == 7) ? false : true;
+    std::cout << isBool;
+
 }
